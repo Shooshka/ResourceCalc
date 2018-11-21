e8dccc35ae0716a7d756b6fe34d205bcc7e13fc4
Vyacheslav Mezetskiy <root@ishoo.ru>
2018-11-21 08:58:55 +0300

    TempFinal

--- ResourceCalc/Form1.cs
+++ ResourceCalc/Form1.cs
@@ -57,7 +57,7 @@ namespace ResourceCalc
                     $"RAM: {ramTotal}{System.Environment.NewLine}" +
                     $"Disk: {systemdisk}Gb система + {basesdisk}Gb под базы.";
             }
-            //Структура Терм и 1с+скуль, ОТ 16 юзеров ДО 70 юзеров.
+            //Структура Терм и 1с+скуль RDP+Web, ОТ 16 юзеров ДО 70 юзеров.
             else if (comboBox1.SelectedIndex == 0 && numericUsers.Value > 15)
             {
                 var cpu4users = (Convert.ToInt32(numericUsers.Value) % 7 == 0) ? Convert.ToInt32(numericUsers.Value) / 7 : (Convert.ToInt32(numericUsers.Value) / 7) + 1;
@@ -69,7 +69,7 @@ namespace ResourceCalc
                 var cpuTotalTerm = cpu4users + cpu4system;
                 var cpuTotal1c = cpu4system + cpu41c + cpu4sql;
 
-                var ram4users = (Convert.ToInt32(numericUsers.Value) % 2 == 0) ? Convert.ToInt32(numericUsers.Value) / 2 : (Convert.ToInt32(numericUsers.Value) / 2) + 1; ;
+                var ram4users = (Convert.ToInt32(numericUsers.Value) % 2 == 0) ? Convert.ToInt32(numericUsers.Value) / 2 : (Convert.ToInt32(numericUsers.Value) / 2) + 1;
                 var ram4system = 2;
 
                 var ram41c = cpu41c * 4;
@@ -91,7 +91,63 @@ namespace ResourceCalc
                     $"RAM 1c+sql: {ramTotal1c}{System.Environment.NewLine}" +
                     $"Disk: {systemdisk1c}Gb система + {basesdisk1c}Gb под базы.";
             }
+            //3-в-1, Web, ДО 80 юзеров И база ДО 24Гб включительно И суммарно баз ДО 24Гб включительно.
+            else if (comboBox1.SelectedIndex == 1 && numericUsers.Value < 81 && numericBasesGB.Value < 25 && numericMaxBaseGB.Value < 25)
+            {
+                var cpu4users = 1;
+                var cpu4system = 1;
+                var cpu41c = (Convert.ToInt32(numericUsers.Value) % 20 == 0) ? Convert.ToInt32(numericUsers.Value) / 20 : (Convert.ToInt32(numericUsers.Value) / 20) + 1;
+                var cpu4sql = 1 + cpu41c;
+                var cpuTotal = cpu4users + cpu41c + cpu4sql + cpu4system;
 
+                var ram4users = 2;
+                var ram4system = 2;
+                var ram41c = cpu41c * 4;
+                var ram4sql = numericBasesGB.Value / 2;
+                var ramTotal = ram4users + ram4system + ram41c + ram4sql;
+
+                var systemdisk = 60;
+                var basesdisk = Convert.ToDouble(numericBasesGB.Value) * 1.5;
+
+                resultBox.Text = $"Сажаем клиента на 3-в-1.{System.Environment.NewLine}{System.Environment.NewLine}" +
+                    $"CPU: {cpuTotal}{System.Environment.NewLine}" +
+                    $"RAM: {ramTotal}{System.Environment.NewLine}" +
+                    $"Disk: {systemdisk}Gb система + {basesdisk}Gb под базы.";
+            }
+            //Структура Терм и 1с+скуль WEB, ОТ 16 юзеров ДО 70 юзеров.
+            else if (comboBox1.SelectedIndex == 1 && numericBasesGB.Value > 24 && numericBasesGB.Value < 51)
+            {
+                var cpu4users = 1;
+                var cpu4system = 1;
+
+                var cpu41c = (Convert.ToInt32(numericUsers.Value) % 20 == 0) ? Convert.ToInt32(numericUsers.Value) / 20 : (Convert.ToInt32(numericUsers.Value) / 20) + 1;
+                var cpu4sql = 1 + cpu41c;
+
+                var cpuTotalTerm = cpu4users + cpu4system;
+                var cpuTotal1c = cpu4system + cpu41c + cpu4sql;
+
+                var ram4users = 2;
+                var ram4system = 2;
+
+                var ram41c = cpu41c * 4;
+                var ram4sql = numericBasesGB.Value / 2;
+
+                var ramTotalTerm = ram4users + ram4system;
+                var ramTotal1c = ram4system + ram41c + ram4sql;
+
+                var systemdiskTerm = 50;
+
+                var systemdisk1c = 60;
+                var basesdisk1c = Convert.ToDouble(numericBasesGB.Value) * 1.5;
+
+                resultBox.Text = $"Сажаем клиента на Терм(Web) и 1с+скуль.{System.Environment.NewLine}{System.Environment.NewLine}" +
+                    $"Term CPU: {cpuTotalTerm}{System.Environment.NewLine}" +
+                    $"Term RAM: {ramTotalTerm}{System.Environment.NewLine}" +
+                    $"Disk Term: {systemdiskTerm}Gb система.{System.Environment.NewLine}{System.Environment.NewLine}" +
+                    $"CPU 1c+sql: {cpuTotal1c}{System.Environment.NewLine}" +
+                    $"RAM 1c+sql: {ramTotal1c}{System.Environment.NewLine}" +
+                    $"Disk: {systemdisk1c}Gb система + {basesdisk1c}Gb под базы.";
+            }
         }
     }
 }
